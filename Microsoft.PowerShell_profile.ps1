864de081f20549736755a4cbbe047d27295f7aaf
Stephen Wan <StephenWan@skyscanner.net>
2017-03-27 17:55:45 +0800

    Remove unused.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,26 +2,17 @@ Import-Module MyModule
 
 New-Alias -name run -value Start-Process -description "Start process" -option ReadOnly
 New-Alias -name emacs -value Start-Emacs -description "Launch emacs" -option ReadOnly
-New-Alias -name styleguide -value Enter-TeleoptiStyleguide -description "Teleopti Styleguide" -option ReadOnly
-New-Alias -name teleopti -value Enter-TeleoptiDebug -description "Teleopti Debug" -option ReadOnly
-New-Alias -name wfm -value Enter-TeleoptiWFM -description "Teleopti WFM" -option ReadOnly
-New-Alias -name vpn -value Enable-TeleoptiVpn -description "Enable Teleopti VPN" -option ReadOnly
-New-Alias -name vpn-off -value Disable-TeleoptiVpn -description "Disable Teleopti VPN" -option ReadOnly
-New-Alias -name restore -value  Start-TeleoptiRestoreToLocal -description "Teleopti Restore to Local" -option ReadOnly
-New-Alias -name challenger -value New-TeleoptiChallenger -description "Teleopti Challenger" -option ReadOnly
-New-Alias -name devbuild -value New-DevBuild -description "Teleopti Devbuild" -option ReadOnly
 New-Alias -name github -value New-GitHub -description "Github" -option ReadOnly
-New-Alias -name pull -value Start-TeleoptiSourcePull -description "Pull Teleopti Source Code" -option ReadOnly
 New-Alias -name explore -value Select-File -description "Select file in explorer" -option ReadOnly
 New-Alias -name find -value Find-HgFile -description "Find Teleopti source file" -option ReadOnly
-New-Alias -name build -value Start-TeleoptiBuild -description "Build Teleopti solution" -option ReadOnly
 New-Alias -name google -value Search-Google -description "Search with Google" -option ReadOnly
 New-Alias -name bing -value Search-Bing -description "Search with Bing" -option ReadOnly
 New-Alias -name desktop -value Enter-Desktop -description "Desktop" -option ReadOnly
 New-Alias -name documents -value Enter-MyDocuments -description "My Documents" -option ReadOnly
 New-Alias -name daily -value Open-DailyWorkbench -description "Open daily workbench" -option ReadOnly
-New-Alias -name door-open -value Open-Door -description "Zhima Kai Men" -option ReadOnly
-New-Alias -name repo -value Enter-Repo -description "Goto repo" -option ReadOnly
+New-Alias -name myrepo -value Enter-MyRepo -description "Goto repo" -option ReadOnly
+New-Alias -name crepo -value Enter-CompanyRepo -description "Goto company repo" -option ReadOnly
+New-Alias -name crepoweb -value New-CompanyRepoWeb -description "Got company repo web" -option ReadOnly
 
 
 function Update-MyModule {
--- Modules/MyMOdule/MyModule.psm1
+++ Modules/MyMOdule/MyModule.psm1
@@ -1,21 +1,12 @@
-$TeleoptiDebug = "$Env:Teleopti\.debug-Setup"
-$TeleoptiWFM = "$Env:Teleopti\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM"
-$TeleoptiStyleguide = "$Env:GitRepo\styleguide"
-$Repo = "$Env:GitRepo"
-$TeleoptiVpn = "typhoon","vpn"
+$CompanyRepo = "$Env:CompanyRepo"
+$CompanyRepoUrl = "$Env:CompanyRepoUrl"
+$MyRepo = "$Env:MyRepo"
+$MyRepoUrl = "$Env:MyRepoUrl"
+
 
 $RunEmacs = " ${env:ProgramFiles(x86)}\GNU\emacs\bin\runemacs.exe"
 $RunChrome = " ${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
-$RunMsbuild = " ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
 
-
-$TeleoptiNodeBuildTarget = "$TeleoptiWFM\..\.node\node.targets"
-$TeleoptiSln = "$TeleoptiDebug\..\CruiseControl.sln"
-$TeleoptiWeb = "$TeleoptiWFM\..\Teleopti.Ccc.Web.csproj"
-$TeleoptiFeatureToggle = "$TeleoptiDebug\..\Domain\FeatureFlags\toggles.txt"
-
-$DoorMAC = "$Env:DoorMAC"
-$DoorIP = "$Env:DoorIP"
 
 function Start-Emacs {
     if ($args.Length -eq 0) {
@@ -26,16 +17,16 @@ function Start-Emacs {
     }
 }
 
-function Enter-TeleoptiDebug {
-    Set-Location $TeleoptiDebug
+function New-CompanyRepoWeb {
+    & $RunChrome "$CompanyRepoUrl"
 }
 
-function Enter-TeleoptiWFM {
-    Set-Location $TeleoptiWFM
+function Enter-MyRepo {
+    Set-Location $MyRepo
 }
 
-function Enter-TeleoptiStyleguide {
-    Set-Location $TeleoptiStyleguide
+function Enter-CompanyRepo {
+    Set-Location $CompanyRepo
 }
 
 function Enter-Desktop {
@@ -46,9 +37,6 @@ function Enter-MyDocuments {
     Set-Location ([environment]::GetFolderPath("MyDocument"))
 }
 
-function Enter-Repo {
-    Set-Location $Repo
-}
 
 function Read-ConsoleColor {
     return @{
@@ -76,111 +64,14 @@ function Open-DailyWorkbench {
     Start-Emacs $dailyWork
 }
 
-function Get-TeleoptiVpn {
-    $interfaceAlias = Get-NetIPAddress | ForEach-Object { $_.InterfaceAlias }
-    foreach ($item in $TeleoptiVpn) {
-        if ($interfaceAlias -contains $item) {
-            Write-Host "Current vpn connection is $item."
-            return $item;
-        }
-    }
-    return;
-}
-
-function Disable-TeleoptiVpn {
-    $current = Get-TeleoptiVpn
-    if ($current -ne $null) {
-        Write-Host "Disconnecting from $current..."
-        rasdial $current /DISCONNECT
-    }
-}
-
-function Enable-TeleoptiVpn {
-    $vpn = Get-TeleoptiVpn
-
-    if ($vpn -eq $null) {
-        Write-Host "No vpn connection is found."
-        $retry = 10;
-        while ($retry -gt 0) {
-            $idx = $retry % $TeleoptiVpn.Length
-            $vpn =  $TeleoptiVpn[$idx]
-            Write-Host "Attempting to connect to $vpn..."
-
-            $p = Start-Process rasdial $vpn -PassThru -Wait
-
-            if ($p.ExitCode -eq 0) {
-                Write-Host "Connected to $vpn."
-                return $vpn
-            } else {
-                Write-Host "Cannot connect to $vpn."
-                $retry -= 1
-            }
-        }
-    } else {
-        Write-Host "Connected to $vpn."
-        return $vpn
-    }
-
-    Write-Error "Cannot connect to $TeleoptiVpns within the retry limit."
-}
-
-function Start-TeleoptiRestoreToLocal {
-    $vpn = Enable-TeleoptiVpn
-    if ($vpn -eq $null) {
-        Write-Error "Aborted! Cannot establish VPN connection."
-        return
-    }
-    Write-Host "Starting Teleopti Restore To Local..."
-
-    $originalColors = Read-ConsoleColor
-    & "$TeleoptiDebug\Restore to Local.bat"
-
-    Update-ConsoleColor($originalColors)
-}
-
-function New-TeleoptiChallenger {
-    $vpn = Enable-TeleoptiVpn
-    if ($vpn -eq $null) {
-        Write-Error "Aborted! Cannot establish VPN connection."
-        return
-    }
-
-    $url = "http://challenger:8080/Kanban/#/board/0"
-    & $RunChrome $url
-    Write-Host "New challenger started in Chrome."
-}
-
-function New-DevBuild {
-    $vpn = Enable-TeleoptiVpn
-    if ($vpn -eq $null) {
-        Write-Error "Aborted! Cannot establish VPN connection."
-        return
-    }
-
-    $url = "http://buildsrv01/project.html?projectId=TeleoptiWFM"
-    & $RunChrome $url
-    Write-Host "New devbuild started in Chrome."
-}
 
 function New-GitHub {
-    Disable-TeleoptiVpn
     $url = "https://github.com"
     & $RunChrome $url
     Write-Host "Github started in Chrome."
 }
 
-function Start-TeleoptiSourcePull {
-    $vpn = Enable-TeleoptiVpn
-    if ($vpn -eq $null) {
-        Write-Error "Aborted! Cannot establish VPN connection."
-        return
-    }
-    Enter-TeleoptiWFM
-    hg pull
-}
-
-
-function open-file($path) {
+function Open-file($path) {
     Start-Process explorer.exe -ArgumentList "/select,$path"
 }
 
@@ -209,62 +100,12 @@ function Select-File {
     }
 }
 
-function Find-HgFile {
-    Enter-TeleoptiWFM
-    if ($args.Length -eq 0) {
-        return
-    }
-    $pattern = $args[0]
-    hg file | Select-String -pattern $pattern | ForEach { Resolve-Path $_ }
-}
-
-function Hide-TeleoptiNodeBuild {
-    (Get-Content $TeleoptiNodeBuildTarget) -replace '(?<!<!--\s*)(<Exec\s.*?/>)(?!\s*-->)','<!--$1-->' | Set-Content -Path $TeleoptiNodeBuildTarget
-    Write-Host "Muted Node-related Execs in Teleopti Build."
-}
-
-function Hide-EtlSpeedUps {
-    (Get-Content $TeleoptiFeatureToggle) -replace '^(ETL_SpeedUp\w+\s+=\s*)(?:RC|true)(.*)$','$1 false $2' | Set-Content -Path $TeleoptiFeatureToggle
-    Write-Host "Muted ETL Speedups."
-}
-
-function Show-TeleoptiNodeBuild {
-    (Get-Content $TeleoptiNodeBuildTarget)  -replace '<!--(<Exec.*?/>)-->','$1' | Set-Content -Path $TeleoptiNodeBuildTarget
-    Write-Host "Restored Node-related Execs in Teleopti Build."
-}
-
-function Start-TeleoptiBuild {
-    if ($args.Length -eq 0) {
-        $startProj = $TeleoptiSln
-    } else {
-        $startProj = $args[0]
-    }
-
-    $originalColors = Read-ConsoleColor
-    Hide-TeleoptiNodeBuild
-    Write-Host "Start building ..."
-    & ($RunMsbuild) /target:build $startProj
-    Show-TeleoptiNodeBuild
-    Update-ConsoleColor($originalColors)
-}
-
 function Search-Google {
     & $RunChrome "https://www.google.com/search?q=$args"
 }
 
-
 function Search-Bing {
     & $RunChrome "https://www.bing.com/search?q=$args"
-}
-
-function Open-Door {
-    wget $DoorIP
-}
-
-function Show-DoorIP {
-    if ((arp -a | Select-String -pattern $DoorMAC) -match "([\d.]+)") {
-        $matches[0]
-    }
 }
 
 
